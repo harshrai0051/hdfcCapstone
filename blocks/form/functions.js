4f2632dd50c16c309546cb0b5d1bb97a4311498e
harshrai0051 <harshr@adobe.com>
2026-05-01 18:27:12 +0530

    Revert "emi calcuation"
    This reverts commit fe4d43d1dd54149157232f6ceaedebb28edf44f5.

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -56,70 +56,7 @@ function maskMobileNumber(mobileNumber) {
   return ` ${'*'.repeat(5)}${value.substring(5)}`;
 }
 
-/**
- * Updates the range bubble position and value
- * @param {HTMLInputElement} input
- * @param {HTMLElement} element
- */
-function updateBubble(input, element) {
-  const step = input.step || 1;
-  const max = input.max || 0;
-  const min = input.min || 1;
-  const value = input.value || 1;
-  const current = Math.ceil((value - min) / step);
-  const total = Math.ceil((max - min) / step);
-  const bubble = element.querySelector('.range-bubble');
-  // during initial render the width is 0. Hence using a default here.
-  const bubbleWidth = bubble.getBoundingClientRect().width || 31;
-  const left = `${(current / total) * 100}% - ${(current / total) * bubbleWidth}px`;
-  bubble.innerText = `${value}`;
-  const steps = {
-    '--total-steps': Math.ceil((max - min) / step),
-    '--current-steps': Math.ceil((value - min) / step),
-  };
-  const style = Object.entries(steps).map(([varName, varValue]) => `${varName}:${varValue}`).join(';');
-  bubble.style.left = `calc(${left})`;
-  element.setAttribute('style', style);
-}
-
-/**
- * Decorate a range field with bubble, min/max labels
- * @param {HTMLElement} fieldDiv
- * @param {object} fieldJson
- * @returns {HTMLElement}
- */
-async function decorate(fieldDiv, fieldJson) {
-  const input = fieldDiv.querySelector('input');
-  // modify the type in case it is not range.
-  input.type = 'range';
-  input.min = input.min || 1;
-  input.max = input.max || 100;
-  input.step = fieldJson?.properties?.stepValue || 1;
-  // create a wrapper div to provide the min/max and current value
-  const div = document.createElement('div');
-  div.className = 'range-widget-wrapper decorated';
-  input.after(div);
-  const hover = document.createElement('span');
-  hover.className = 'range-bubble';
-  const rangeMinEl = document.createElement('span');
-  rangeMinEl.className = 'range-min';
-  const rangeMaxEl = document.createElement('span');
-  rangeMaxEl.className = 'range-max';
-  rangeMinEl.innerText = `${input.min || 1}`;
-  rangeMaxEl.innerText = `${input.max}`;
-  div.appendChild(hover);
-  // move the input element within the wrapper div
-  div.appendChild(input);
-  div.appendChild(rangeMinEl);
-  div.appendChild(rangeMaxEl);
-  input.addEventListener('input', (e) => {
-    updateBubble(e.target, div);
-  });
-  updateBubble(input, div);
-  return fieldDiv;
-}
-
 // eslint-disable-next-line import/prefer-default-export
 export {
-  getFullName, days, submitFormArrayToString, maskMobileNumber, updateBubble, decorate,
+  getFullName, days, submitFormArrayToString, maskMobileNumber,
 };
